c57e93a9fa711a70796ab9d4f6cf9fcbab010c58
Ricky Rhodes <ricky@rickyrhodes.com>
2020-11-19 16:01:08 -0500

    add wikistack sequelize progress

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const layout = require('./views/layout');
+const app = express();
+const { db } = require('./models');
 
+db.authenticate().then(() => console.log('Database initialized...'));
 
-app.use(morgan('dev'))
-// app.use(express.static())
-app.use(express.urlencoded({extended: false}))
-app.get('/', function(req, res){
-    console.log('Hello World')
-})
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
+app.get('/', function (req, res) {
+  res.send(layout(''));
+});
 
-const PORT = 3000
-app.listen(PORT, function(){ 
-    console.log('hi')
-})
+const PORT = 3000;
+
+const init = async () => {
+  await db.sync({ force: true });
+
+  app.listen(PORT, function () {
+    console.log(`Listening on PORT: ${PORT}`);
+  });
+};
+
+init();
